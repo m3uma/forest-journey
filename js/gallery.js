cc6e0871a861f04a3ef6d2d4757b1e19d19da1ad
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-10-01 18:31:24 +0200

    improved styled for mobile screen

--- js/gallery.js
+++ js/gallery.js
@@ -15,10 +15,6 @@ const swiper = new Swiper('.swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-    },
     mousewheel: {
         invert: true,
     },
