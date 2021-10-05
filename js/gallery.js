c5b7d5f7b3af6dbf0a0f74b2d7e1fc3fb333a27a
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-10-05 17:08:30 +0200

    automatic generate gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,26 @@
+const imgArray = [
+    './img/gallery/forest.jpg',
+    './img/gallery/forest2.jpg',
+    './img/gallery/forest3.jpg',
+    './img/gallery/forest4.jpg',
+    './img/gallery/autumn_forest.jpg',
+    './img/gallery/autumn_forest2.jpg',
+    './img/gallery/autumn_forest3.jpg',
+    './img/gallery/winter_forest.jpg',
+]
+
+const swiperWrapper = document.querySelector(".swiper-wrapper");
+
+for (let image in imgArray){
+    const slide = document.createElement('div');
+    slide.classList.add('swiper-slide');
+    const imageSlide = document.createElement('img');
+    imageSlide.setAttribute('src', imgArray[image]);
+    slide.appendChild(imageSlide);
+    swiperWrapper.appendChild(slide);
+}
+
+
 const swiper = new Swiper('.swiper', {
     autoplay: true,
     centeredSlides: true,
