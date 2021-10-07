c84553e18ef415128198d7e15950d4134881eb45
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-10-07 17:15:27 +0200

    added logs with values

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+const userName = document.querySelector('.name-js');
+const userEmail = document.querySelector('.mail-js');
+const userMessage = document.querySelector('.message-js');
+const sendButton = document.querySelector('.form__button');
+
+sendButton.onclick = (event) => {
+    event.preventDefault();
+    console.log(`User: ${userName.value}, E-mail: ${userEmail.value}, Message: ${userMessage.value}`);
+}
