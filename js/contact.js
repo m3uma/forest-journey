51d1084d8d7905a515b29698d134333495e3594a
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-10-08 15:50:35 +0200

    added sending messages

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,23 @@
-const userName = document.querySelector('.name-js');
-const userEmail = document.querySelector('.mail-js');
-const userMessage = document.querySelector('.message-js');
-const sendButton = document.querySelector('.form__button');
+document.querySelector('.form__button').onclick = (event) => {
+    const userName = document.querySelector('.name-js').value;
+    const userEmail = document.querySelector('.mail-js').value;
+    const userMessage = document.querySelector('.message-js').value;
 
-sendButton.onclick = (event) => {
     event.preventDefault();
-    console.log(`User: ${userName.value}, E-mail: ${userEmail.value}, Message: ${userMessage.value}`);
+    console.log(`User: ${userName}, E-mail: ${userEmail}, Message: ${userMessage}`);
     document.querySelector('.form').reset();
+
+    sendEmail(userName, userEmail, userMessage);
+}
+
+function sendEmail(name, email, message) {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "dev.test.mail669@gmail.com",
+        Password: "DevTestMail669",
+        To: 'dev.test.mail669@gmail.com',
+        From: `${email}`,
+        Subject: `${name} send you a message`,
+        Body: `Name: ${name} <br /> Email: ${email} <br /> Message: ${message}`,
+    }).then(() => alert("Message sent successfully!"));
 }
